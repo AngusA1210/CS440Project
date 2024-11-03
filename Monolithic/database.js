56c913df84651db7a0897b5c1153676da9fbfe1d
AngusA1210 <101908863+AngusA1210@users.noreply.github.com>
2024-11-02 19:00:33 -0700

    Add main monolithic project directory

---
+++ Monolithic/database.js
@@ -0,0 +1,21 @@
+// Database uses SQLite
+
+const sqlite3 = require('sqlite3').verbose();
+
+// Save database as a file
+const db = new sqlite3.Database('./database.db');
+
+db.serialize(() => {
+    db.run('CREATE TABLE items (id INTEGER PRIMARY KEY, title TEXT, cost REAL, description TEXT)');
+});
+
+// Database functions
+function getItems(callback) {
+    db.all('SELECT * FROM items', callback);
+}
+
+function addItem(title, cost, description, callback) {
+    db.run('INSERT INTO items (title, cost, description) VALUES (?, ?, ?)', [title, cost, description], callback);
+}
+
+module.exports = { getItems, addItem };
